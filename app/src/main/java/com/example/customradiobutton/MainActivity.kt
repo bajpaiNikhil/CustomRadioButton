86c823bb292be9d441b8499163299d23cc99a4a3
Gaurav Bajpai <gaurav.tcs15@gmail.com>
2021-06-20 10:10:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/customradiobutton/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.customradiobutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
